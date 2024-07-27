d3085ff7b9d7221db0f5467e091d0f60a77358e5
Janos Hardi <janos.hardi@gmail.com>
2024-07-27 12:19:18 +0200

    refactoring, new languages

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,35 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("You!")
-	want := "Hello You!"
+	t.Run("saying hello to people", func(t *testing.T) {
+		got := Hello("You!", "")
+		want := "Hello, You!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("say 'Hello, world!' when an empty string is supplied", func(t *testing.T) {
+		got := Hello("", "")
+		want := "Hello, World!"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Spanish", func(t *testing.T) {
+		got := Hello("Elodie", spanish)
+		want := "Hola, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in French", func(t *testing.T) {
+		got := Hello("Elodie", french)
+		want := "Bonjour, Elodie"
+		assertCorrectMessage(t, got, want)
+	})
+	t.Run("in Hungarian", func(t *testing.T) {
+		got := Hello("Peti", hungarian)
+		want := "Szevasz, Peti"
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
 	}
